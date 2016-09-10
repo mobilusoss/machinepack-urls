4a99eaf05c44af249f3c7eb9781b6a9dd7bd675b
Mike McNeil <mike@balderdash.co>
2016-09-10 17:37:06 -0500

    Write up lots more documentation.

--- machines/resolve.js
+++ machines/resolve.js
@@ -7,7 +7,21 @@ module.exports = {
   description: 'Build a sanitized, fully-qualified version of the provided URL.',
 
 
-  extendedDescription: 'Given a URL, returns a fully-qualified URL with trailing slashes stripped off.  For example, if a valid protocol is provided (e.g. "https://") and the original URL contains no trailing slashes, the URL returned will be identical to what was passed in.  If the provided URL begins with "//", it will be replaced with "http://".  If the provided URL does not start with a usable protocol, "http://" will be prepended.  If the URL cannot be sanitized, the `error` exit will be triggered.',
+  extendedDescription:
+  'Given a URL, this returns a fully-qualified URL with trailing slashes stripped off.  It also infers a protocol, if necessary.  '+
+  'For example, if a valid protocol is provided (e.g. "https://") and the original URL contains no trailing slashes, the URL '+
+  'returned will be identical to what was passed in.  If the provided URL begins with "//", it will be replaced with "http://".  '+
+  'If the provided URL does not start with a usable protocol (e.g. "google.com"), then "http://" will be prepended.  If the URL '+
+  'cannot be sanitized (e.g. if it is missing a hostname, or altogether malformed), then the `error` exit will be triggered.  '+
+  'In other words, if `/foo/bar` is passed in as the URL (and assuming the optional base URL is not provided), then this machine '+
+  'will fail.\n'+
+  '\n'+
+  '### Resolving relative to a base URL\n'+
+  'Optionally, this machine _also_ allows a base URL (`baseUrl`) to be provided.  This allows a URL path (like `/foo/bar`) to be '+
+  'provided as the primary URL, as long as a valid URL with a hostname is provided as the base URL.  To stick with our example from '+
+  'above, if `/foo/bar` is passed in as the primary URL (`url`), and a valid base URL-- say, `api.example.com/pets` _is also provided_, '+
+  'then, instead of failing, this machine will return `http://api.example.com/pets/foo/bar`.'+
+  '',
 
 
   sync: true,
@@ -21,7 +35,7 @@ module.exports = {
     url: {
       friendlyName: 'URL',
       description: 'The URL to resolve, with or without the protocol prefix (e.g. "http://").',
-      extendedDescription: 'If a `baseUrl` is specified, then this URL should be specified as a URL path (e.g. "/foo").',
+      extendedDescription: 'If a `baseUrl` is specified, then this URL should be specified as a URL path (e.g. "/foo").  Otherwise, this _must_ include the hostname (e.g. `api.example.com`).',
       example: 'www.example.com/search',
       required: true
     },
@@ -30,7 +44,7 @@ module.exports = {
       friendlyName: 'Base URL',
       description: 'Optional base URL to resolve against, with or without the protocol prefix (e.g. "http://").',
       extendedDescription: 'If specified, this _must_ include the hostname (e.g. `api.example.com`).  It may also include a path (e.g. `http://api.example.com/pets`).',
-      example: 'https://api.example.com/pets'
+      example: 'api.example.com/pets'
     }
 
   },
